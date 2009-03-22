c7ea4f159c511e4502120474b40b52ca5dce2c6e
bla <bla@2bf9390a-0bcf-47c0-9393-da87f60a85e0>
2009-03-22 13:05:11 +0000

    Little done; some comments/rethinking/correcting.
    git-svn-id: http://thera.be/svn/UZK1/trunk@7 2bf9390a-0bcf-47c0-9393-da87f60a85e0

--- Protocol/Modbus.h
+++ Protocol/Modbus.h
@@ -4,17 +4,28 @@
 #include "Protocol.h"
 #include "Lowlevel/Lowlevel.h"
 
+
+/** Implements modbus ascii protocol */
 class Modbus : public Protocol
 {
 protected:
 	/** Callback passed to Modbus from interface 
 	 * we will inform user about new messages 
-	 * via this interface.
+	 * by calling it's methods.
 	 */
-	Protocol::Callback &C;
+	Protocol::Callback *C;
+
+	/** Lowlevel class created and configured in Interface
+	 * We store it, and pass it a callback. */
 	Lowlevel &L;
+
+	/* Lowlevel callback implementation */
+	class LowlevelCallback : public Lowlevel::Callback
+	{
+	};
+
 public:
-	Modbus(Callback &C, Lowlevel &LL);
+	Modbus(Callback *C, Lowlevel &LL);
 	virtual void RegisterCallback(Callback &C);
 	virtual void SendMessage(const std::string &Msg, int Address = 0);
 
