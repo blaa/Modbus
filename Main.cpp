b31538f62f2266e1b534517cdc6038b802c76fcc
bla <bla@2bf9390a-0bcf-47c0-9393-da87f60a85e0>
2009-04-27 07:23:08 +0000

    Compiles
    git-svn-id: http://thera.be/svn/UZK1/trunk@33 2bf9390a-0bcf-47c0-9393-da87f60a85e0

--- Main.cpp
+++ Main.cpp
@@ -336,7 +336,6 @@ namespace Testcase {
 			}
 		} else {
 			for (;;) {
-				P->SendMessage(s, 'A', 'F');
 				P->SendMessage("SERVER", 'A', 'F');
 
 				Timeout::Sleep(1000); /* Receiver will bug this */
